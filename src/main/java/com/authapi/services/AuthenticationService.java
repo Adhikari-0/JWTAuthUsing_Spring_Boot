75d6d74af3ab9a4834cd3ccecfa1b1833cdfafd2
Krishna <Nepaliclimate@gmail.com>
2024-09-17 15:12:20 +0545

    Initial commit

---
+++ src/main/java/com/authapi/services/AuthenticationService.java
@@ -0,0 +1,44 @@
+package com.authapi.services;
+
+import com.authapi.dtos.LoginUserDto;
+import com.authapi.dtos.RegisterUserDto;
+import com.authapi.entities.User;
+import com.authapi.repositories.UserRepository;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+	private final UserRepository userRepository;
+
+	private final PasswordEncoder passwordEncoder;
+
+	private final AuthenticationManager authenticationManager;
+
+	public AuthenticationService(UserRepository userRepository, AuthenticationManager authenticationManager,
+			PasswordEncoder passwordEncoder) {
+		this.authenticationManager = authenticationManager;
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	public User signup(RegisterUserDto input) {
+
+		User user = new User();
+		user.setFullName(input.getFullName());
+		user.setEmail(input.getEmail());
+		user.setPassword(passwordEncoder.encode(input.getPassword()));
+
+		return userRepository.save(user);
+	}
+
+	public User authenticate(LoginUserDto input) {
+		authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(input.getEmail(), input.getPassword()));
+
+		return userRepository.findByEmail(input.getEmail()).orElseThrow();
+	}
+}
