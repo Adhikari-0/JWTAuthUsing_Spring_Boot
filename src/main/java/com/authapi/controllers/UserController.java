60eac84ea27ea310c21a3a5f90772c600b078728
Krishna <Nepaliclimate@gmail.com>
2024-09-25 16:14:48 +0545

    updated

--- src/main/java/com/authapi/controllers/UserController.java
+++ src/main/java/com/authapi/controllers/UserController.java
@@ -30,7 +30,7 @@ public class UserController {
 		return ResponseEntity.ok(currentUser);
 	}
 
-	@GetMapping("/")
+	@GetMapping("/all")
 	public ResponseEntity<List<User>> allUsers() {
 		List<User> users = userService.allUsers();
 
--- src/test/java/com/authapi/JWTAuthApplicationTests.java
+++ src/test/java/com/authapi/JWTAuthApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tericcabrel.authapi;
+package com.authapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
